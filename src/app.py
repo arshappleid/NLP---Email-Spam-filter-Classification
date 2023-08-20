0ba1083ec7ed7cf40e24ed37152b75d4a678ae8d
arshappleid <arsh.appleid@gmail.com>
2023-08-20 09:59:40 -0700

    Added dev , test and prod environments

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods = ['GET'])
 def hello():
-    		return "Format this"
+	return jsonify({"status": "ping"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
